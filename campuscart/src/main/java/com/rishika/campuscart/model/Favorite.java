81d5c303a31b6ba5006191bfcb0832fc74620d2d
rishikanigam2006315 <rishikanigam315@gmail.com>
2026-05-03 04:28:56 +0530

    id issue resolved

--- campuscart/src/main/java/com/rishika/campuscart/model/Favorite.java
+++ campuscart/src/main/java/com/rishika/campuscart/model/Favorite.java
@@ -1,11 +1,7 @@
 package com.rishika.campuscart.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
 @Entity
 @Table(name = "favorites")
