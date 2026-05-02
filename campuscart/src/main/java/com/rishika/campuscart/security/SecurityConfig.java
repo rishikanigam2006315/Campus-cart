22ed5313b78e1bf55070146921dd465a6f186e4b
rishikanigam2006315 <rishikanigam315@gmail.com>
2026-05-03 01:56:04 +0530

    Security config

--- campuscart/src/main/java/com/rishika/campuscart/security/SecurityConfig.java
+++ campuscart/src/main/java/com/rishika/campuscart/security/SecurityConfig.java
@@ -5,23 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//        http
-//                .csrf(csrf -> csrf.disable())
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/api/auth/**").permitAll()
-//                        .anyRequest().permitAll()
-//                );
-//
-//        return http.build();
-//    }
-//}
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -39,5 +24,10 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.disable());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
